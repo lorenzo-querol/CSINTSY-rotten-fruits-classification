7148ea894f0eedcd0de9bd8c4c25ce5a5f8c014f
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2023-04-15 17:28:17 +0800

    Update segmentation.py

--- segmentation.py
+++ segmentation.py
@@ -4,7 +4,6 @@ import numpy as np
 from alive_progress import alive_bar
 import matplotlib.pyplot as plt
 
-
 def segment_image(image):
     rgb_image = cv2.cvtColor(cv2.imread(image), cv2.COLOR_BGR2RGB)
 
@@ -19,7 +18,7 @@ def segment_image(image):
         blur_image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
     # Apply mask on image
-    masked_image = cv2.bitwise_and(rgb_image, thresh)
+    masked_image = cv2.bitwise_and(rgb_image, rgb_image, mask=thresh)
 
     plt.imshow('image', masked_image)
     plt.show()
@@ -31,3 +30,14 @@ test_image = f'{train_dir}/freshapples/rotated_by_15_Screen Shot 2018-06-08 at 4
 
 
 segment_image(test_image)
+
+
+def plot_examples(images):
+    fig, axs = plt.subplots(6, 5)
+    fig.set_size_inches(15, 15)
+    for i in range(6):
+        for j in range(5):
+            axs[i, j].imshow(images[i*5+j])
+            axs[i, j].axis('off')
+    plt.show()
+    
